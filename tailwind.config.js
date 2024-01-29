fd4ffc0e25e2186c99e25957f747901704a5b41a
Tenon <143541798+TenonCzar@users.noreply.github.com>
2024-01-30 00:45:40 +0100

    social proof

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    colors: {
+      VeryDarkMagenta: 'hsl(300, 43%, 22%)',
+SoftPink: 'hsl(333, 80%, 67%)',
+      DarkGrayishMagenta: 'hsl(303, 10%, 53%)',
+LightGrayishMagenta: 'hsl(300, 24%, 96%)',
+White: 'hsl(0, 0%, 100%)',
+    },
+
+    fontFamily: {
+      'league': ['League Spartan', 'sans-serif']
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
